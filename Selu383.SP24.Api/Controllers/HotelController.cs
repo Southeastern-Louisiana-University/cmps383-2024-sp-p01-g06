21b411714f87ca90f1d9b77892f8fb5a1b6cb1ff
Joshua Cantu <joshua@thecantu.com>
2024-01-17 22:44:03 -0600

    Added hotel entity

---
+++ Selu383.SP24.Api/Controllers/HotelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Selu383.SP24.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/hotels")]
+    public class HotelController
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 5;
+        }
+
+    }
+}
