8c1e7cdb77b985fdf1bceee189d5eb69e027f0aa
Joshua Cantu <joshua@thecantu.com>
2024-01-22 20:33:39 -0600

    Added deafult string

--- Selu383.SP24.Api/Entities/Hotel.cs
+++ Selu383.SP24.Api/Entities/Hotel.cs
@@ -3,8 +3,8 @@
     public class Hotel
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
     }
 
 
