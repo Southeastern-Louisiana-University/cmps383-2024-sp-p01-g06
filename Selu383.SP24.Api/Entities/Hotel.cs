e4af06c75a1699876e564409dc3e9af565b65114
Joshua Cantu <joshua@thecantu.com>
2024-01-22 22:17:33 -0600

    Created hotel Dto

--- Selu383.SP24.Api/Entities/Hotel.cs
+++ Selu383.SP24.Api/Entities/Hotel.cs
@@ -7,5 +7,12 @@
         public string Address { get; set; } = string.Empty;
     }
 
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+
 
 }
